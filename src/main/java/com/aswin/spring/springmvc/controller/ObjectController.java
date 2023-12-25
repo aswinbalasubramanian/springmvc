878e5f1d28b1a91e7673ca7ceaf73b0a67d3a832
Aswin <aswin.balasubramanian@agnity.com>
2023-12-25 20:53:05 +0530

    created object class and controller to send it

---
+++ src/main/java/com/aswin/spring/springmvc/controller/ObjectController.java
@@ -0,0 +1,24 @@
+package com.aswin.spring.springmvc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.aswin.spring.springmvc.dto.Employee;
+
+public class ObjectController {
+	@RequestMapping("/readObject")
+	public ModelAndView sendObject()
+	{
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("displayObject");
+		
+		Employee employee = new Employee();
+		employee.setId(1);
+		employee.setName("Connor");
+		employee.setSalary(10500);
+		
+		modelAndView.addObject("employee",employee);
+		
+		return modelAndView;
+	}
+}
