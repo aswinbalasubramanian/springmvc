bea95b6e402d28c2d77ebc63467479b26b88351f
Aswin <aswin.balasubramanian@agnity.com>
2023-12-25 20:57:13 +0530

    view data from object controller

--- src/main/java/com/aswin/spring/springmvc/controller/ObjectController.java
+++ src/main/java/com/aswin/spring/springmvc/controller/ObjectController.java
@@ -1,10 +1,11 @@
 package com.aswin.spring.springmvc.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.aswin.spring.springmvc.dto.Employee;
-
+@Controller
 public class ObjectController {
 	@RequestMapping("/readObject")
 	public ModelAndView sendObject()
